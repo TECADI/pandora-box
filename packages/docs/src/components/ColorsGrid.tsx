9943a6271637d2f116c643de68c0b995b1563438
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-05-08 18:16:46 -0300

    add anew presentation in colors

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -3,19 +3,35 @@ import { getContrast } from 'polished'
 import '../styles/colors-grid.css'
 
 export const ColorsGrid = () => {
+  const colorsPalletList = Object.entries(colors).map(([key, color]) => {
+    return {
+      key,
+      color,
+      contrast: getContrast(color, '#fff') > 3.5 ? '#fff' : '#000',
+    }
+  })
+
+  const filteredBlackAndWhite = colorsPalletList.filter(
+    // eslint-disable-next-line prettier/prettier
+    ({ key }) => key !== 'black' && key !== 'white'
+  )
+
   return (
     <div className="colors-grid">
-      {Object.entries(colors).map(([key, color]) => (
-        <div
-          key={key}
-          className="color-card"
-          style={{
-            backgroundColor: color,
-            color: getContrast(color, '#fff') > 3.5 ? '#fff' : '#000',
-          }}
-        >
-          <strong>${key}</strong>
-          <span>{color}</span>
+      {filteredBlackAndWhite.map(({ key, color, contrast }) => (
+        <div key={key} className="divider">
+          <div
+            className="color-card"
+            style={{
+              backgroundColor: color,
+              color: contrast,
+            }}
+          ></div>
+
+          <div className="color-content">
+            <strong>{key}</strong>
+            <span>{color}</span>
+          </div>
         </div>
       ))}
     </div>
