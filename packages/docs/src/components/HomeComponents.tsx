605fd891cf04574bad7f8f503359ed3dfde01f8b
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-05-09 08:44:07 -0300

    add img component in homepage

--- packages/docs/src/components/HomeComponents.tsx
+++ packages/docs/src/components/HomeComponents.tsx
@@ -1,3 +1,7 @@
 export function Divider({ space }: { space: string }) {
   return <div style={{ padding: space }} />
 }
+
+export function Image({ src, alt }: { src: string; alt: string }) {
+  return <img src={src} alt={alt} />
+}
