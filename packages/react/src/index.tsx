4f3bb6e5e1bc7c8b83274f8151e84499c046b2fe
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-05-28 14:28:14 -0300

    TEC-2439 | update: exportation components

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,14 @@
-export * from './components/Box'
-export * from './components/Text'
-export * from './components/Heading'
-export * from './components/Avatar'
 export * from './components/Button'
 export * from './components/TextInput'
-export * from './components/TextArea'
-export * from './components/Checkbox'
-export * from './components/MultiStep'
 export * from './components/Header'
-export * from './components/Container'
 export * from './components/Alert'
+
+// new components
+export * from './components/Flex'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/InputGroup'
+export * from './components/BreadCrumb'
+
+// new styles
 export * from './styles'
