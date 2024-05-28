0f7dc4e143f9ca0fbdd0108ca628d9bfd5b2115f
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-05-28 17:12:13 -0300

    TEC-2421 | fix: fix new configs in button component

--- packages/react/src/components/Button/index.tsx
+++ packages/react/src/components/Button/index.tsx
@@ -1,39 +1,25 @@
-/*
-import { ComponentProps, ElementType } from 'react'
 import { ButtonContainer, LoadingSpinner } from './styles'
-
-export interface ButtonProps extends ComponentProps<typeof ButtonContainer> {
-  as?: ElementType
-  loading?: boolean
-}
-
-export function Button({ loading, ...props }: ButtonProps) {
-  return (
-    <ButtonContainer {...props} disabled={loading || props.disabled}>
-      {loading ? (
-        <>
-          <LoadingSpinner />
-          Carregando
-        </>
-      ) : (
-        props.children
-      )}
-    </ButtonContainer>
-  )
-}
-
-Button.displayName = 'Button'
-
-*/
-
-import { ButtonContainer } from './styles'
 
 export interface ButtonProps {
   children: React.ReactNode
-  variant?: 'success' | 'danger' | 'warning' | 'info'
+  variant?: 'primary' | 'success' | 'danger' | 'warning' | 'info'
   size?: 'sm' | 'md' | 'lg'
+  disabled?: boolean
+  loading?: boolean
+  onClick?: () => void
 }
 
-export function Button({ children, variant }: ButtonProps) {
-  return <ButtonContainer variant={variant}>{children}</ButtonContainer>
+export function Button({ children, loading, disabled, ...props }: ButtonProps) {
+  return (
+    <ButtonContainer {...props} disabled={loading || disabled}>
+      {loading ? (
+        <>
+          <LoadingSpinner size={props.size} />
+          Carregando
+        </>
+      ) : (
+        children
+      )}
+    </ButtonContainer>
+  )
 }
