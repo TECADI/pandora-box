0f0bd05d62cd69db0928bdab61d1ff3e84ba8b00
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-05-28 18:16:40 -0300

    TEC-2440 | feature: create new component

---
+++ packages/tokens/src/sizes.ts
@@ -0,0 +1,16 @@
+export const sizes: {
+  [key: string]: { fontSize: string; padding: string }
+} = {
+  sm: {
+    fontSize: '14px',
+    padding: '6px 10px',
+  },
+  md: {
+    fontSize: '16px',
+    padding: '8px 14px',
+  },
+  lg: {
+    fontSize: '18px',
+    padding: '10px 18px',
+  },
+}
