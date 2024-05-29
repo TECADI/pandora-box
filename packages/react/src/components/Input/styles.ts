37b3e7e5c84875b7b04cb06348006ca84f1ea07d
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-05-29 15:27:40 -0300

    TEC-2422 | feature: create default input

---
+++ packages/react/src/components/Input/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '../../styles'
+import { InputProps } from '.'
+
+export const InputComponent = styled.input<InputProps>`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+`
