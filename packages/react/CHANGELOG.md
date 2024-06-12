90996f63a086aff4de63cfbee4a9d5f9571b47be
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2024-06-12 09:01:56 -0300

    fix versions change

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,71 @@
 # @pandora-box-tecadi/desing-ui-react
 
+## 4.1.7
+
+### Patch Changes
+
+- up
+
+## 4.1.6
+
+### Patch Changes
+
+- up versions
+
+## 4.1.5
+
+### Patch Changes
+
+- as
+
+## 4.1.4
+
+### Patch Changes
+
+- up
+
+## 4.1.3
+
+### Patch Changes
+
+- table full size
+
+## 4.1.2
+
+### Patch Changes
+
+- update
+
+## 4.1.1
+
+### Patch Changes
+
+- test
+
+## 4.1.0
+
+### Minor Changes
+
+- update size components
+
+## 4.0.0
+
+### Major Changes
+
+- desc
+
+## 3.0.0
+
+### Major Changes
+
+- test
+
+## 2.0.0
+
+### Major Changes
+
+- fix prop component
+
 ## 1.0.2
 
 ### Patch Changes
--- packages/react/src/utils/getVariants.ts
+++ packages/react/src/utils/getVariants.ts
@@ -1,10 +1,21 @@
+import { colors } from '@pandora-box-tecadi/design-system'
 import { theme } from '../styles'
 import { Variants } from './types'
+import { getContrast } from 'polished'
 
 interface VariantProps {
-  variant?: Variants
+  colorSchema?: Variants
 }
 
-export const getVariant = ({ variant = 'primary' }: VariantProps) => {
-  return theme.variants[variant]
+export const getVariant = ({ colorSchema = 'primary' }: VariantProps) => {
+  return theme.variants[colorSchema]
+}
+
+export const getContrastColor = (colorSchema: VariantProps) => {
+  const bgColor = getVariant(colorSchema).backgroundColor
+  const textColor = colors.slate100
+
+  const contrastValue = getContrast(bgColor, textColor)
+
+  return contrastValue < 2.5 ? colors.slate700 : colors.slate50
 }
